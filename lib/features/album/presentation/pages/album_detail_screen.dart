640befb667ded58e625fa3779d32b2338aecfbe4
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2025-05-21 15:25:55 +0300

    Refactor AlbumDetailScreen layout to improve image error handling and rearrange album details display

--- lib/features/album/presentation/pages/album_detail_screen.dart
+++ lib/features/album/presentation/pages/album_detail_screen.dart
@@ -15,6 +15,22 @@ class AlbumDetailScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Center(
+              child: Image.network(
+                album.url,
+                height: 200,
+                width: 200,
+                fit: BoxFit.cover,
+                errorBuilder:
+                    (context, error, stackTrace) => Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.broken_image, size: 200),
+                        Text("Image could not be loaded"),
+                      ],
+                    ),
+              ),
+            ),
             Text('Album ID: ${album.id}', style: _headerStyle),
             const SizedBox(height: 8),
             Text('Album Title: ${album.title}', style: _subHeaderStyle),
@@ -23,16 +39,6 @@ class AlbumDetailScreen extends StatelessWidget {
             Text('Photo Title: ${album.photoTitle}', style: _normalStyle),
             Text('Photo Album ID: ${album.albumId}', style: _normalStyle),
             const SizedBox(height: 16),
-            Center(
-              child: Image.network(
-                album.url,
-                height: 200,
-                width: 200,
-                fit: BoxFit.cover,
-                errorBuilder:
-                    (_, __, ___) => const Text("Image could not be loaded"),
-              ),
-            ),
           ],
         ),
       ),
