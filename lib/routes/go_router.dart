205cc111c8e87fb33d7f2041b65bc59994d5d144
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2025-05-21 14:50:53 +0300

    Refactor main.dart to integrate Bloc for album fetching and update go_router.dart with album navigation routes

--- lib/routes/go_router.dart
+++ lib/routes/go_router.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:music_app/features/album/domain/entities/album_with_photos/album_with_photos.dart';
+import 'package:music_app/features/album/presentation/pages/album_detail_screen.dart';
+import 'package:music_app/features/album/presentation/pages/album_list_screen.dart';
 
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/',
+  debugLogDiagnostics: true,
+  routes: [
+    GoRoute(
+      path: '/',
+      name: 'albumList',
+      builder: (BuildContext context, GoRouterState state) {
+        return AlbumListScreen();
+      },
+    ),
+    GoRoute(
+      path: '/detail',
+      name: 'albumDetail',
+      builder: (BuildContext context, GoRouterState state) {
+        final album = state.extra as AlbumWithPhoto;
+        return AlbumDetailScreen(album: album);
+      },
+    ),
+  ],
+);
