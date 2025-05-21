446f56223bce6d3d6af898f5955e9ccf9fd004f3
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2025-05-21 14:50:35 +0300

    Add AlbumDetailScreen and AlbumListScreen for album display and navigation

---
+++ lib/features/album/presentation/pages/album_detail_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:music_app/features/album/domain/entities/album_with_photos/album_with_photos.dart';
+
+class AlbumDetailScreen extends StatelessWidget {
+  final AlbumWithPhoto album;
+
+  const AlbumDetailScreen({Key? key, required this.album}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Album Details')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Album ID: ${album.id}', style: _headerStyle),
+            const SizedBox(height: 8),
+            Text('Album Title: ${album.title}', style: _subHeaderStyle),
+            const SizedBox(height: 16),
+            Text('Photo ID: ${album.photoId}', style: _normalStyle),
+            Text('Photo Title: ${album.photoTitle}', style: _normalStyle),
+            Text('Photo Album ID: ${album.albumId}', style: _normalStyle),
+            const SizedBox(height: 16),
+            Center(
+              child: Image.network(
+                album.url,
+                height: 200,
+                width: 200,
+                fit: BoxFit.cover,
+                errorBuilder:
+                    (_, __, ___) => const Text("Image could not be loaded"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  TextStyle get _headerStyle =>
+      const TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+  TextStyle get _subHeaderStyle =>
+      const TextStyle(fontSize: 16, fontWeight: FontWeight.w500);
+  TextStyle get _normalStyle => const TextStyle(fontSize: 14);
+}
---
+++ lib/features/album/presentation/pages/album_list_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:music_app/features/album/presentation/bloc/album_bloc.dart';
+
+class AlbumListScreen extends StatefulWidget {
+  @override
+  _AlbumListScreenState createState() => _AlbumListScreenState();
+}
+
+class _AlbumListScreenState extends State<AlbumListScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AlbumBloc>().add(FetchAlbumsEvent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Albums")),
+      body: BlocBuilder<AlbumBloc, AlbumState>(
+        builder: (context, state) {
+          if (state is AlbumLoading) {
+            return Center(child: CircularProgressIndicator());
+          } else if (state is AlbumLoaded) {
+            final albums = state.albums;
+            return ListView.builder(
+              itemCount: albums.length,
+              itemBuilder: (context, index) {
+                final album = albums[index];
+                return ListTile(
+                  leading: Image.network(
+                    album.thumbnailUrl,
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
+                    errorBuilder:
+                        (context, error, stackTrace) =>
+                            Icon(Icons.broken_image, size: 50),
+                  ),
+                  title: Text(
+                    album.title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: Text('Tap for details'),
+                  onTap: () {
+                    context.pushNamed('albumDetail', extra: album);
+                  },
+                );
+              },
+            );
+          } else if (state is AlbumError) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(state.message),
+                  ElevatedButton(
+                    onPressed: () {
+                      context.read<AlbumBloc>().add(FetchAlbumsEvent());
+                    },
+                    child: Text("Retry"),
+                  ),
+                ],
+              ),
+            );
+          }
+          return Container();
+        },
+      ),
+    );
+  }
+}
